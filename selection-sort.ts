3a464fc3ff407f5275bc46588f1f4581befcdc5e
Harshada <67410367+harshadapr@users.noreply.github.com>
2023-11-05 23:10:45 +0530

    Create selection-sort.ts

---
+++ selection-sort.ts
@@ -0,0 +1,27 @@
+// selection-sort.ts
+
+function selectionSort(arr: number[]): number[] {
+  const n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      const temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+
+  return arr;
+}
+
+const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+const sortedArray = selectionSort(unsortedArray);
+console.log(sortedArray);
